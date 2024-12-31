94244519cc58f50b71dc463c3b5571c74c9a63a6
Ishan Taunk <Ishan.Taunk@Quantum.com>
2024-12-31 09:50:49 +0530

    quantum final change

--- src/App.js
+++ src/App.js
@@ -59,13 +59,13 @@ function App() {
                     <div className="about-me-text-header">Hi, I am Ishan Taunk</div>
                     <div>As a Frontend Web Developer I specialise in creating functional and beautiful websites and web applications that are optimised for user experience. My expertise lies in HTML, CSS, JavaScript, Angular and React.</div>
                     <div className='btn-section'>
-                        <a href={Resume} download="Ishan_Taunk_Resume.pdf" className='btn-text'><button className='btn'>Download Resume</button></a>
+                        <a href={Resume} download="Ishan_Taunk_Resume.pdf"><button className='btn'>Download Resume</button></a>
                     </div>
                 </div>
             </div>
             <div className="row">
                 <div className="we-image image-sec">
-                    <img className="inst-image" src={inst} alt="inst.png"></img>
+                    <img className="inst-image" src={inst} alt="inst.png" loading='lazy'></img>
                 </div>
                 <div className="work-experience-timeline text-sec">
                     <div className="about-me-text-header">Work</div>
